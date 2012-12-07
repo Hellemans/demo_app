53df789185be300724add08db17bd5dcc27d1f0a
Sven Hellemans <sven@eventdrive.be>
2012-12-07 13:05:43 +0100

    Inital commit demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '6b07efebdcfb7a4682e12f3c0ab924f0f9d58766911cbed33070e25f23ee6f4fa8c5d6a93ce28c2b3553085536954eb5801c5dccb4e547f4f1ce74bcf314bbe4'
